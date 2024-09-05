c8c6d0cf329770881c4ac7ab1febe5e5b4649a18
René Lebherz <me@renelebherz.com>
2024-09-05 10:37:53 +0200

    chore: add gradle.properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,6 @@ plugins {
 	id("buildlogic.version-conventions")
 }
 
-group = "io.github.bitfist"
-description = "Markdown Editor component for JavaFX"
-
 dependencies {
 	implementation("org.apache.commons:commons-lang3:3.17.0")
 	implementation("de.jensd:fontawesomefx-fontawesome:4.7.0-9.1.2")
